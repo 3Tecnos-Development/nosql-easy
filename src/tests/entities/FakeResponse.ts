146b78730a6749b741afbe63bf3687446a360275
moirocar <moirocar@gmail.com>
2021-01-11 16:38:24 -0300

    refactor: remove properties commented

--- src/tests/entities/FakeResponse.ts
+++ src/tests/entities/FakeResponse.ts
@@ -3,12 +3,4 @@ import { Expose } from "class-transformer";
 export class FakeResponse {
   @Expose()
   id: string;
-  // @Expose()
-  // age: number;
-
-  // @Expose()
-  // email: string;
-
-  // @Expose()
-  // name: string;
 }
