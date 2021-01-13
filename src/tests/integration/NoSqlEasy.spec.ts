7374943abecb33d0109e973274ffdb9ac1b6b6f7
fasjunior <fabiano.asj@gmail.com>
2021-01-13 15:47:58 -0300

    test: include the getPaginatedArray test and adjust the others with the custom response class

--- src/tests/integration/NoSqlEasy.spec.ts
+++ src/tests/integration/NoSqlEasy.spec.ts
@@ -1,16 +1,24 @@
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable no-plusplus */
 /* eslint-disable import/no-extraneous-dependencies */
 import "reflect-metadata";
 import dotenv from "dotenv";
-import { plainToClass } from "class-transformer";
+import faker from "faker";
 import { NoSqlEasyConfig } from "../../Config";
 import { DialectType } from "../../types/DialectType";
 import { NoSqlEasy } from "../../index";
-import { FakeFilter, FakeResponse } from "../entities";
+import { FakeFilter, FakeItemResponse, FakeResponse } from "../entities";
 import { OrderBy } from "../../types";
 
 dotenv.config();
 
 jest.setTimeout(30000);
+
+interface IFakeItem {
+  itemId: string;
+  value: number;
+  total: number;
+}
 
 interface IFake {
   id?: string;
@@ -18,11 +26,38 @@ interface IFake {
   age: number;
   email: string;
   isDad?: boolean;
+  items: IFakeItem[];
 }
 
 NoSqlEasyConfig.setDialect(process.env.DB_DIALECT as DialectType);
 
 let dynamicId: string;
+let insertTestId: string;
+
+const mockFake = (id: string): FakeResponse => {
+  return {
+    id,
+  };
+};
+
+const mockFakeItem = (value: number): FakeItemResponse => {
+  return {
+    value,
+  };
+};
+
+const mockItems = (size: number = 10): IFakeItem[] => {
+  const data: IFakeItem[] = [];
+  for (let i = 0; i < size; i++) {
+    const f: IFakeItem = {
+      itemId: faker.random.uuid(),
+      value: faker.random.number(),
+      total: faker.random.number(),
+    };
+    data.push(f);
+  }
+  return data;
+};
 
 describe("NoSqlEasy", () => {
   it("Testando o método insert", async () => {
@@ -30,6 +65,7 @@ describe("NoSqlEasy", () => {
       name: "Lindsay",
       age: 36,
       email: "lindsay@3tecnos.com.br",
+      items: [],
     };
     const newFake = await new NoSqlEasy().insert<IFake>("fakes", fake);
     dynamicId = newFake.id!;
@@ -49,6 +85,7 @@ describe("NoSqlEasy", () => {
       age: 33,
       email: "jesus@3tecnos.com.br",
       isDad: true,
+      items: mockItems(20),
     };
     const newFake = await noSqlEasy.insertWithId<IFake>("fakes", fake);
     const exists = await noSqlEasy.exists("fakes", newFake.id!);
@@ -109,6 +146,7 @@ describe("NoSqlEasy", () => {
       age: 36,
       email: "lindsay@3tecnos.com.br",
       isDad: true,
+      items: [],
     };
     const noSqlEasy = new NoSqlEasy();
     await noSqlEasy.update<IFake>("fakes", fakeDad);
@@ -138,23 +176,22 @@ describe("NoSqlEasy", () => {
 
   it("Testando o método insert com o retorno customizado", async () => {
     const fake: IFake = {
-      id: "000000",
       name: "Moises",
       age: 42,
       email: "moirocar@gmail.com",
+      items: [],
     };
 
     const noSqlEasy = new NoSqlEasy();
 
-    const id = await noSqlEasy.insertWithId("fakes", fake, FakeResponse);
+    const response = await noSqlEasy.insert("fakes", fake, FakeResponse);
+    insertTestId = response.id;
 
-    const toCompare = plainToClass(
-      FakeResponse,
-      { id, ...fake },
-      { excludeExtraneousValues: true },
-    );
+    const toCompare: FakeResponse = {
+      id: insertTestId,
+    };
 
-    expect(fake).toMatchObject(toCompare);
+    expect(response).toEqual(toCompare);
   });
 
   it("Testando o método insertWithId com o retorno customizado", async () => {
@@ -165,36 +202,32 @@ describe("NoSqlEasy", () => {
       age: 33,
       email: "jesus@3tecnos.com.br",
       isDad: true,
+      items: mockItems(20),
     };
-    const id = await noSqlEasy.insertWithId("fakes", fake, FakeResponse);
+    const response = await noSqlEasy.insertWithId("fakes", fake, FakeResponse);
 
-    const toCompare = plainToClass(
-      FakeResponse,
-      { id, ...fake },
-      { excludeExtraneousValues: true },
-    );
+    const toCompare: FakeResponse = {
+      id: response.id,
+    };
 
-    expect(fake).toMatchObject(toCompare);
+    expect(response).toEqual(toCompare);
   });
 
   it("Testando o método getCollection com o retorno customizado", async () => {
-    const response = await new NoSqlEasy().getCollection<FakeResponse>("fakes");
-    const toCompare = await new NoSqlEasy().getCollection<FakeResponse>(
+    const response = await new NoSqlEasy().getCollection<FakeResponse>(
       "fakes",
       undefined,
       FakeResponse,
     );
 
-    expect(response).toMatchObject(toCompare);
+    const objectResponse = response.find((i) => i.id === "123456");
+    const toCompare = mockFake(objectResponse?.id!);
+
+    expect(objectResponse).toEqual(toCompare);
   });
 
   it("Testando o método getPaginatedCollection com o retorno customizado", async () => {
-    const response = await new NoSqlEasy().getPaginatedCollection("fakes", {
-      limit: 5,
-      page: 1,
-    });
-
-    const toCompare = await new NoSqlEasy().getPaginatedCollection(
+    const response = await new NoSqlEasy().getPaginatedCollection(
       "fakes",
       {
         limit: 5,
@@ -204,18 +237,17 @@ describe("NoSqlEasy", () => {
       undefined,
       FakeResponse,
     );
-    expect(response).toMatchObject(toCompare);
+
+    const objectResponse = response.find((i) => i.id === "123456");
+    const toCompare = mockFake(objectResponse?.id!);
+
+    expect(objectResponse).toEqual(toCompare);
   });
 
   it("Testando o método getByValue com retorno customizado", async () => {
     const noSqlEasy = new NoSqlEasy();
-    const fake = await noSqlEasy.getByValue<IFake>(
-      "fakes",
-      "email",
-      "jesus@3tecnos.com.br",
-    );
 
-    const toCompare = await noSqlEasy.getByValue<IFake, FakeResponse>(
+    const response = await noSqlEasy.getByValue<IFake, FakeResponse>(
       "fakes",
       "email",
       "jesus@3tecnos.com.br",
@@ -223,35 +255,83 @@ describe("NoSqlEasy", () => {
       FakeResponse,
     );
 
-    expect(fake).toMatchObject(toCompare);
+    const toCompare = mockFake(response[0].id!);
+
+    expect(response[0]).toEqual(toCompare);
   });
 
   it("Testando o método getByValueOrdered com retorno customizado", async () => {
     const noSqlEasy = new NoSqlEasy();
-    const fake = await noSqlEasy.getByValueOrdered<IFake>(
-      "fakes",
-      "age",
-      ">=",
-      30,
-      "age",
-      "asc",
-    );
 
-    const toCompare = await noSqlEasy.getByValueOrdered<IFake, FakeResponse>(
+    const response = await noSqlEasy.getByValueOrdered<IFake, FakeResponse>(
       "fakes",
-      "age",
-      ">=",
-      30,
+      "email",
+      "==",
+      "jesus@3tecnos.com.br",
       "age",
       "asc",
       FakeResponse,
     );
 
-    expect(fake).toMatchObject(toCompare);
+    const toCompare = mockFake(response[0].id!);
+
+    expect(response[0]).toEqual(toCompare);
+  });
+
+  it("Testando o método getById com retorno customizado", async () => {
+    const noSqlEasy = new NoSqlEasy();
+
+    const response = await noSqlEasy.getById<IFake, FakeResponse>(
+      "fakes",
+      "123456",
+      FakeResponse,
+    );
+    const toCompare = mockFake(response.id!);
+
+    expect(response).toEqual(toCompare);
+  });
+
+  it("Testando o método getPaginatedArray com retorno customizado", async () => {
+    const noSqlEasy = new NoSqlEasy();
+
+    const response = await noSqlEasy.getPaginatedArray<
+      IFake,
+      IFakeItem,
+      FakeItemResponse
+    >("fakes", "123456", "items", 1, 5, FakeItemResponse);
+
+    const toCompare = mockFakeItem(response[0].value);
+
+    expect(response[0]).toEqual(toCompare);
+  });
+
+  it("Testando o método getPaginatedArray", async () => {
+    const noSqlEasy = new NoSqlEasy();
+    const pageSize = 5;
+    const pageNumber = 1;
+    const lastIndex = pageNumber * pageSize - 1;
+
+    const arrayPaginated = await noSqlEasy.getPaginatedArray<IFake, IFakeItem>(
+      "fakes",
+      "123456",
+      "items",
+      pageNumber,
+      pageSize,
+    );
+
+    const fake = await noSqlEasy.getById<IFake>("fakes", "123456");
+    const toCompare = fake.items;
+
+    expect(toCompare).toEqual(expect.arrayContaining(arrayPaginated));
+    expect(arrayPaginated.length).toEqual(pageSize);
+    expect(toCompare[lastIndex]).toMatchObject(
+      arrayPaginated[arrayPaginated.length - 1],
+    );
   });
 
   it("Testando o método remove", async () => {
     const noSqlEasy = new NoSqlEasy();
+    await noSqlEasy.remove("fakes", insertTestId);
     await noSqlEasy.remove("fakes", dynamicId);
     await noSqlEasy.remove("fakes", "123456");
     await noSqlEasy.remove("fakes", "000000");
