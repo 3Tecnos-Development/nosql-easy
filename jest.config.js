1757a66b5248bd354ed810981757f09d86d5fe27
fasjunior <fabiano.asj@gmail.com>
2021-01-13 15:43:08 -0300

    chore: adjust the path of the jest test files

--- jest.config.js
+++ jest.config.js
@@ -141,7 +141,7 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/tests/**/*.spec.ts?(x)"],
+  testMatch: ["**/*.spec.ts?(x)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
