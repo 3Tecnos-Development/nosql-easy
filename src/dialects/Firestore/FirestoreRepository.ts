b7ce9a4899944072efc2d109833d293180e02a98
fasjunior <fabiano.asj@gmail.com>
2021-04-29 17:53:56 -0300

    feat: includes filter conditional

--- src/dialects/Firestore/FirestoreRepository.ts
+++ src/dialects/Firestore/FirestoreRepository.ts
@@ -1,74 +1,40 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable @typescript-eslint/indent */
-/* eslint-disable indent */
-/* eslint-disable class-methods-use-this */
 /* eslint-disable no-undef */
-/* eslint-disable prefer-promise-reject-errors */
 /* eslint-disable no-prototype-builtins */
+/* eslint-disable indent */
+/* eslint-disable @typescript-eslint/indent */
 /* eslint-disable import/no-unresolved */
-import { MapEnv } from "map-env-node";
+
 import firebase, { firestore } from "firebase-admin";
 import { DocumentData } from "@firebase/firestore-types";
 
-import { IFirestoreCredential } from "./interfaces";
 import { IDataTransformPort, IRepository } from "../../interfaces";
 import { FieldNested, Options, Where } from "../../types";
 import {
   emptyToUndefined,
+  orderArrayBy,
   paginateArray,
+  removeUndefinedProps,
   transformFirestoreTypes,
 } from "../../helpers";
 import { DataTransformAdapter } from "../../adapters/dataTransformer";
+import {
+  addOrderByTo,
+  docToModel,
+  filterToWhere,
+  getConditionalQueries,
+  getCredentials,
+  intersectModels,
+} from "./helpers";
 
 export class FirestoreRepository implements IRepository {
   private firestore: firestore.Firestore;
 
   private dataTransform: IDataTransformPort;
 
-  private getCredentials = (): IFirestoreCredential => {
-    let projectId: string;
-    let clientEmail: string;
-    let privateKey: string;
-    let databaseURL: string;
-
-    if (process.env.FIRESTORE_CREDENTIAL) {
-      const firestoreCredential = MapEnv.get<IFirestoreCredential>(
-        "FIRESTORE_CREDENTIAL",
-      );
-
-      projectId = firestoreCredential.credential.projectId;
-      clientEmail = firestoreCredential.credential.clientEmail;
-      privateKey = firestoreCredential.credential.privateKey;
-      databaseURL = firestoreCredential.databaseURL;
-    } else {
-      projectId = process.env.FIRESTORE_PROJECT_ID!;
-      clientEmail = process.env.FIRESTORE_CLIENT_EMAIL!;
-      if (process.env.FIRESTORE_PRIVATE_KEY_BASE64) {
-        const privateKeyB64 = Buffer.from(
-          process.env.FIRESTORE_PRIVATE_KEY_BASE64,
-          "base64",
-        );
-        privateKey = privateKeyB64.toString("utf8");
-      } else {
-        privateKey = process.env.FIRESTORE_PRIVATE_KEY!;
-      }
-      databaseURL = process.env.FIRESTORE_DATABASE_URL!;
-    }
-
-    return {
-      credential: {
-        projectId,
-        clientEmail,
-        privateKey: privateKey.replace(/\\n/g, "\n"),
-      },
-      databaseURL,
-    };
-  };
-
   constructor() {
     this.dataTransform = DataTransformAdapter;
     if (!firebase.apps.length) {
-      const firestoreCredential = this.getCredentials();
+      const firestoreCredential = getCredentials();
       const { credential } = firestoreCredential;
       firebase.initializeApp({
         credential: firebase.credential.cert({
@@ -82,35 +48,17 @@ export class FirestoreRepository implements IRepository {
     this.firestore = firebase.firestore();
   }
 
-  private docToModel<T>(
-    snapShot: firestore.QuerySnapshot<firestore.DocumentData>,
-  ): T[] {
-    const elems = [] as T[];
-    if (!snapShot.empty) {
-      snapShot.forEach((doc) => {
-        const elem = { id: doc.id, ...(doc.data() as T) };
-        elems.push(elem);
-      });
-    }
-    return elems;
-  }
-
-  private removeUndefinedProps<T>(data: T): T {
-    const obj = data as any;
-    Object.keys(obj).forEach((key) => {
-      if (obj[key] === undefined) {
-        delete obj[key];
-        return;
-      }
-      const propertyIsAnObject =
-        obj[key] && typeof obj[key] === "object" && !(obj[key] instanceof Date);
-      if (propertyIsAnObject) {
-        this.removeUndefinedProps(obj[key]);
-        const objectWithoutProperties = !Object.keys(obj[key]).length;
-        if (objectWithoutProperties) delete obj[key];
-      }
-    });
-    return data;
+  private async transform<T, R = T>(
+    value: R[] | firestore.QuerySnapshot<firestore.DocumentData>,
+    ClassType?: new () => R,
+  ): Promise<R[]> {
+    const result: R[] =
+      value instanceof firestore.QuerySnapshot ? docToModel<R>(value) : value;
+    return ClassType
+      ? this.dataTransform.transform(ClassType, result, {
+          excludeExtraneousValues: true,
+        })
+      : transformFirestoreTypes(result);
   }
 
   async insert<T, R = T>(
@@ -119,7 +67,7 @@ export class FirestoreRepository implements IRepository {
     ResponseClass?: new () => R,
   ): Promise<R> {
     const obj = await this.dataTransform.toObject(data);
-    this.removeUndefinedProps(obj);
+    removeUndefinedProps(obj);
     let response = {} as R;
 
     await this.firestore
@@ -145,7 +93,7 @@ export class FirestoreRepository implements IRepository {
     ResponseClass?: new () => R,
   ): Promise<R> {
     const obj = await this.dataTransform.toObject(data);
-    this.removeUndefinedProps(obj);
+    removeUndefinedProps(obj);
 
     if (obj.hasOwnProperty("id")) {
       await this.firestore
@@ -160,7 +108,7 @@ export class FirestoreRepository implements IRepository {
         : transformFirestoreTypes(obj as R);
       return response;
     }
-    return Promise.reject("Id property not provided.");
+    return Promise.reject(new Error("Id property not provided."));
   }
 
   async insertElementInArray(
@@ -169,7 +117,7 @@ export class FirestoreRepository implements IRepository {
     arrayFieldName: string,
     value: any,
   ): Promise<void> {
-    this.removeUndefinedProps(value);
+    removeUndefinedProps(value);
     const docRef = await this.firestore.collection(collection).doc(id);
     await docRef.update(
       arrayFieldName,
@@ -183,7 +131,7 @@ export class FirestoreRepository implements IRepository {
     arrayFieldName: string,
     value: any,
   ): Promise<void> {
-    this.removeUndefinedProps(value);
+    removeUndefinedProps(value);
     const docRef = await this.firestore.collection(collection).doc(id);
     await docRef.update(
       arrayFieldName,
@@ -201,45 +149,58 @@ export class FirestoreRepository implements IRepository {
     options?: Options<T>,
     ResponseClass?: new () => R,
   ): Promise<R[]> {
-    let query = this.firestore.collection(collection) as firestore.Query;
+    const collectionRef = this.firestore.collection(collection);
+    let query = collectionRef as firestore.Query;
 
     if (options) {
-      if (options.whereCollection) {
-        options.whereCollection.forEach((where: any) => {
-          const fieldPath = where.fieldPath
-            ? where.fieldPath
-            : `${where.fieldParent}.${where.fieldNested}`;
-          query = query.where(
-            fieldPath,
-            where.operator.toString() as FirebaseFirestore.WhereFilterOp,
-            where.value,
-          );
-        });
-      }
+      const {
+        whereCollection,
+        filterCollection,
+        orderByCollection,
+        limit,
+        offset,
+      } = options;
 
-      if (options.orderByCollection) {
-        options.orderByCollection.forEach((orderBy) => {
-          query = query.orderBy(
-            orderBy.fieldPath as string,
-            orderBy.direction as FirebaseFirestore.OrderByDirection,
-          );
-        });
-      }
+      const hasConditionals =
+        (whereCollection as any[])?.length > 0 ||
+        (filterCollection as any[])?.length > 0;
 
-      query = options.limit ? query.limit(options.limit) : query;
-      query = options.offset ? query.offset(options.offset) : query;
+      if (hasConditionals) {
+        // Where
+        const queries = getConditionalQueries(collectionRef, [
+          ...(whereCollection ?? []),
+          ...filterToWhere(filterCollection ?? []),
+        ]);
+
+        const hasCompoundQueries = queries.length > 1;
+        if (hasCompoundQueries) {
+          const promises = queries.map((q) => q.get());
+          const snapShots = await Promise.all(promises);
+          const result = await this.transform(
+            intersectModels<R>(snapShots),
+            ResponseClass,
+          );
+          // Order By to Compound Queries
+          if (orderByCollection)
+            orderArrayBy<R>(result, orderByCollection as any);
+
+          // Limit and Offset to Compound Queries
+          const endIndex = limit && offset ? limit + offset : limit;
+          return result.slice(offset, endIndex);
+        }
+        query = queries?.[0];
+      }
+      // Order By
+      query = orderByCollection
+        ? addOrderByTo(query, orderByCollection)
+        : query;
+
+      // Limit and Offset
+      query = limit ? query.limit(limit) : query;
+      query = offset ? query.offset(offset) : query;
     }
-
     const snapShot = await query.get();
-    let elements = this.docToModel<R>(snapShot);
-
-    elements = ResponseClass
-      ? await this.dataTransform.transform(ResponseClass, elements, {
-          excludeExtraneousValues: true,
-        })
-      : transformFirestoreTypes(elements);
-
-    return elements;
+    return this.transform(snapShot, ResponseClass);
   }
 
   async getById<T, R = T>(
@@ -271,7 +232,7 @@ export class FirestoreRepository implements IRepository {
       .collection(collection)
       .where(fieldPath, operator, value)
       .get();
-    let elements = this.docToModel<R>(snapShot);
+    let elements = docToModel<R>(snapShot);
     elements = ResponseClass
       ? await this.dataTransform.transform(ResponseClass, elements, {
           excludeExtraneousValues: true,
@@ -283,7 +244,7 @@ export class FirestoreRepository implements IRepository {
   async getByValueOrdered<T, R = T>(
     collection: string,
     fieldPath: string,
-    whereFilter: FirebaseFirestore.WhereFilterOp = "==",
+    whereFilter: FirebaseFirestore.WhereFilterOp,
     value: any,
     fieldOrder: string,
     direction: FirebaseFirestore.OrderByDirection = "desc",
@@ -294,7 +255,7 @@ export class FirestoreRepository implements IRepository {
       .where(fieldPath, whereFilter, value)
       .orderBy(fieldOrder, direction)
       .get();
-    let elements = this.docToModel<R>(snapShot);
+    let elements = docToModel<R>(snapShot);
     elements = ResponseClass
       ? await this.dataTransform.transform(ResponseClass, elements, {
           excludeExtraneousValues: true,
@@ -305,7 +266,7 @@ export class FirestoreRepository implements IRepository {
 
   async update<T>(collection: string, data: T): Promise<void> {
     const obj = await this.dataTransform.toObject(data);
-    this.removeUndefinedProps(obj);
+    removeUndefinedProps(obj);
     if (obj.hasOwnProperty("id")) {
       const snapShot = this.firestore
         .collection(collection)
@@ -332,7 +293,7 @@ export class FirestoreRepository implements IRepository {
 
       if (typeof value === "object") {
         obj = await this.dataTransform.toObject(value);
-        this.removeUndefinedProps(obj);
+        removeUndefinedProps(obj);
       } else obj = value;
 
       if ((field as FieldNested<T, C>).parent) {
@@ -374,9 +335,7 @@ export class FirestoreRepository implements IRepository {
         );
       });
     }
-
     const snapShot = await query.get();
-
     return snapShot.size;
   }
 
@@ -441,7 +400,7 @@ export class FirestoreRepository implements IRepository {
     const doc = await this.getById<T>(collection, id);
     const fieldValue = doc ? ((doc as any)[field] as []) : [];
     if (!Array.isArray(fieldValue))
-      return Promise.reject("The field is not an array.");
+      return Promise.reject(new Error("The field is not an array."));
 
     const arrayPaginated =
       minimumSizeToPaginated && fieldValue.length < minimumSizeToPaginated
