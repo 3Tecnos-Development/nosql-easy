fe562d74b9203f6bd7bba87f26c862b1d9ea2e62
fasjunior <fabiano.asj@gmail.com>
2021-04-29 17:48:42 -0300

    feat: includes filter types

--- src/types/Where.ts
+++ src/types/Where.ts
@@ -1,4 +1,21 @@
-export declare type WhereFilterOp = "<" | "<=" | "==" | ">=" | ">" | "array-contains" | "in" | "array-contains-any";
+/* eslint-disable @typescript-eslint/indent */
+
+export const UniqueClausesArray = [
+  "array-contains",
+  "in",
+  "array-contains-any",
+] as const;
+export const ComparisonsArray = ["<", "<=", ">=", ">", "!="] as const;
+export const EqualOperatorOp = "==" as const;
+
+export declare type UniqueClauses = typeof UniqueClausesArray[number];
+
+export declare type Comparisons = typeof ComparisonsArray[number];
+
+export declare type EqualOperator = typeof EqualOperatorOp;
+
+export declare type WhereFilterOp = EqualOperator | Comparisons | UniqueClauses;
+
 interface IWhere<K> {
   fieldPath: K;
   operator: WhereFilterOp;
@@ -13,4 +30,11 @@ interface IWhereNested<Parent, Child> {
   value: any;
 }
 
-export declare type WhereNested<Parent, Child> = IWhereNested<keyof Parent, keyof Child>;
+export declare type WhereNested<Parent, Child> = IWhereNested<
+  keyof Parent,
+  keyof Child
+>;
+
+export type WhereGroup<T> = {
+  [field: string]: Where<T>[];
+};
