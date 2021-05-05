5df16fad6831a72f52c23097d09cf81641ba4f1c
fasjunior <fabiano.asj@gmail.com>
2021-05-05 11:12:35 -0300

    feat: check order by of comparisons

--- src/dialects/Firestore/helpers/add-order-by-to/add-order-by-to.ts
+++ src/dialects/Firestore/helpers/add-order-by-to/add-order-by-to.ts
@@ -1,8 +1,18 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-undef */
 import { firestore } from "firebase-admin";
-import { OrderBy, Where, WhereNested } from "../../../../types";
+import {
+  ComparisonsArray,
+  OrderBy,
+  Where,
+  WhereNested,
+} from "../../../../types";
+import { filterConditionsByOperators } from "../filter-conditions-by-operators/filter-conditions-by-operators";
 
+/* 
+  The Firestore does not allow to sort and filter, in the same query, with certain operators.
+  See more: https://firebase.google.com/docs/firestore/query-data/order-limit-data#limitations 
+*/
 const rejectQueryIfOrderByIsNotInAgreement = <T>(
   orderField: keyof T,
   whereCollection: (Where<T> | WhereNested<T, any>)[],
@@ -20,12 +30,46 @@ const rejectQueryIfOrderByIsNotInAgreement = <T>(
     );
 };
 
+/*
+  If there is a filter with a range comparison (<, <=, >, >=), your first ordering must be on the same field. 
+  If the query does not contain the sort, it is included.
+  See more: https://firebase.google.com/docs/firestore/query-data/order-limit-data#limitations
+*/
+const checkOrderByOfComparisons = <T>(
+  orderByCollection: OrderBy<T>[],
+  whereCollection: (Where<T> | WhereNested<T, any>)[],
+): OrderBy<T>[] => {
+  const whereWithComparisons = filterConditionsByOperators(
+    whereCollection,
+    ComparisonsArray,
+  );
+  const missingOrders = whereWithComparisons.reduce(
+    (orders: OrderBy<T>[], where: Where<T>) => {
+      const hasOrderForComparison =
+        orderByCollection
+          .map((order) => order.fieldPath)
+          .includes(where.fieldPath) ||
+        orders.some((order) => order.fieldPath === where.fieldPath);
+
+      !hasOrderForComparison &&
+        orders.push({ fieldPath: where.fieldPath, direction: "asc" });
+      return orders;
+    },
+    [],
+  );
+  return [...missingOrders, ...orderByCollection];
+};
+
 export const addOrderByTo = <T>(
   query: firestore.Query,
   orderByCollection: OrderBy<T>[],
   whereCollection?: (Where<T> | WhereNested<T, any>)[],
 ): firestore.Query => {
-  orderByCollection.forEach((orderBy) => {
+  const order = whereCollection
+    ? checkOrderByOfComparisons(orderByCollection, whereCollection)
+    : orderByCollection;
+
+  order.forEach((orderBy) => {
     const { fieldPath, direction } = orderBy;
     !!whereCollection &&
       rejectQueryIfOrderByIsNotInAgreement(fieldPath, whereCollection);
